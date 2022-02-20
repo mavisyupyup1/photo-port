7661bdd50f45f752499915214ef4c5df01c926c8
mavisyupyup1 <mavisyupyup1@github.com>
2022-02-20 10:12:40 -0500

    add close function to modal

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -104,11 +104,11 @@ const PhotoList = ({category})=>{
       const currentPhotos = photos.filter((photo)=>photo.category===category)
       const toggleModal =(image,i)=>{
         setCurrentPhoto({...image, index:i})
-        setIsModelOpen(true)
+        setIsModelOpen(!isModalOpen)
       }
     return(
         <div>
-          {isModalOpen && <Modal currentPhoto={currentPhoto}/>}
+          {isModalOpen && (<Modal currentPhoto={currentPhoto} onClose={toggleModal}/>)}
             <div className="flex-row">
                 {currentPhotos.map((image,i)=>(
                     <img
